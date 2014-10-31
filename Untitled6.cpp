b9645f3e3b80e5d45eaa8fd79dfe73a4729d6e2c
Elica Ilieva <elica95@mail.bg>
2014-10-31 09:01:54 +0200

    uiku

---
+++ Untitled6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int favoriteNumber;
+    cout << "What is your favorite number?" << endl;
+    cin >> favoriteNumber;
+    cout << "Oh, " << favoriteNumber << " is my favorite number, too!" << endl;
+    return 0;
+
+}
